cbd5c95c983aabcc9971f420919f459ecd059d14
SankarN-19 <nsankar1912@gmail.com>
2025-05-03 16:46:28 +0530

    Integrated API to display product details

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,85 @@
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+import { Typography, Spinner } from "neetoui";
+import { isNotNil, append } from "ramda";
+
 import Carousel from "./Carousel";
-import { IMAGE_URLS } from "./constants";
 
-const Product = () => (
-  <div className="px-6 pb-6">
-    {/* rest of the code */}
-    <div className="w-2/5">
-      <Carousel imageUrls={IMAGE_URLS} title="Infinix Inbook" />
+const Product = () => {
+  const [product, setProduct] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchProduct = async () => {
+    try {
+      const response = await axios.get(
+        "https://smile-cart-backend-staging.neetodeployapp.com/products/infinix-inbook-2"
+      );
+      setProduct(response.data);
+    } catch (error) {
+      console.log("An error occurred:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
+  const {
+    name,
+    description,
+    mrp,
+    offer_price: offerPrice,
+    image_urls: imageUrls,
+    image_url: imageUrl,
+  } = product;
+
+  const totalDiscounts = mrp - offerPrice;
+  const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
+
+  if (isLoading) {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <Spinner />
+      </div>
+    );
+  }
+
+  return (
+    <div className="px-6 pb-6">
+      <Typography className="py-2 text-4xl font-semibold" style="h1">
+        {name}
+      </Typography>
+
+      <div className="mt-16 flex gap-4">
+        <div className="w-2/5">
+          <div className="flex justify-center gap-16">
+            {isNotNil(imageUrls) ? (
+              <Carousel imageUrls={append(imageUrl, imageUrls)} title={name} />
+            ) : (
+              <img alt={name} className="w-48" src={imageUrl} />
+            )}
+          </div>
+        </div>
+
+        <div className="w-3/5 space-y-4">
+          <Typography>{description}</Typography>
+
+          <Typography>MRP: {mrp}</Typography>
+
+          <Typography className="font-semibold">
+            Offer Price: {offerPrice}
+          </Typography>
+
+          <Typography className="font-semibold text-green-600">
+            {discountPercentage}% off
+          </Typography>
+        </div>
+      </div>
     </div>
-
-    {/* rest of the code */}
-  </div>
-);
+  );
+};
 
 export default Product;
