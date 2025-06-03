c5282b32238a28a5647466fe487e7c5a2ab62151
SankarN-19 <nsankar1912@gmail.com>
2025-06-03 20:06:03 +0530

    Implemented localization to smile cart

--- src/components/Cart/PriceCard.jsx
+++ src/components/Cart/PriceCard.jsx
@@ -1,7 +1,7 @@
 import classNames from "classnames";
 import { Button, Typography } from "neetoui";
 import { gt, keys } from "ramda";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import routes from "routes";
 import useCartItemsStore from "stores/useCartItemsStore";
 
@@ -20,26 +20,33 @@ const PriceCard = ({ totalMrp, totalOfferPrice }) => {
           "line-through": isDiscountPresent,
         })}
       >
-        Total MRP:
-        <span>${totalMrp}</span>
+        <Trans
+          components={{ typography: <span /> }}
+          i18nKey="totalMrp"
+          values={{ mrp: totalMrp }}
+        />
       </Typography>
 
       {isDiscountPresent && (
         <>
           <Typography className="flex justify-between text-green-700">
-            Total discounts:
-            <span>
-              ${totalDiscounts} ({discountPercentage}%)
-            </span>
+            <Trans
+              components={{ span: <span /> }}
+              i18nKey="totalDiscounts"
+              values={{ discounts: totalDiscounts, discountPercentage }}
+            />
           </Typography>
 
           <Typography className="flex justify-between">
-            Total offer price:
-            <span>${totalOfferPrice}</span>
+            <Trans
+              components={{ span: <span /> }}
+              i18nKey="offerPrice"
+              values={{ offerPrice: totalOfferPrice }}
+            />
           </Typography>
 
           <span className="neeto-ui-text-gray-500 text-sm">
-            {itemsCount} item(s)
+            {t("itemCount", { count: itemsCount })}
           </span>
         </>
       )}
